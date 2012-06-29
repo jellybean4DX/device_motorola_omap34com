2270ec48e54d18b3495c6c0fdd3061aea019c54c
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-06-29 10:06:42 -0400

    rename to libaudio and add audio.policy.omap3

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -60,3 +60,18 @@ LOCAL_MODULE := libaudiopolicy_legacy223
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= AudioPolicyManager.cpp
+LOCAL_MODULE:= audio_policy.omap3
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES:= liaudiopolicy_legacy libmedia_helper
+LOCAL_SHARED_LIBRARIES:= libcutils libutils libmedia
+
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+  LOCAL_CFLAGS += -DWITH_A2DP
+endif
+
+include $(BUILD_SHARED_LIBRARY)
